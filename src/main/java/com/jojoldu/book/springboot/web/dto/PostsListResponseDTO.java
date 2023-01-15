74f6cfd999a5fd9e9101dd372a2f4ac5f72222fd
JUNHYUNG CHO <junhyungcho@JUNHYUNGui-MacBookPro.local>
2023-01-15 16:17:34 +0900

    게시판 기능 구현

--- src/main/java/com/jojoldu/book/springboot/web/dto/PostsListResponseDTO.java
+++ src/main/java/com/jojoldu/book/springboot/web/dto/PostsListResponseDTO.java
@@ -3,18 +3,20 @@ package com.jojoldu.book.springboot.web.dto;
 import com.jojoldu.book.springboot.domain.posts.Posts;
 import lombok.Getter;
 
-@Getter
-public class PostsResponseDTO {
+import java.time.LocalDateTime;
 
+@Getter
+public class PostsListResponseDTO {
   private Long id;
   private String title;
-  private String content;
   private String author;
+  private LocalDateTime modifiedDate;
 
-  public PostsResponseDTO(Posts entity) {
+  public PostsListResponseDTO(Posts entity) {
     this.id = entity.getId();
     this.title = entity.getTitle();
-    this.content = entity.getContent();
     this.author = entity.getAuthor();
+    this.modifiedDate = entity.getModifiedDate();
   }
+
 }
--- src/main/java/com/jojoldu/book/springboot/web/dto/PostsUpdateRequestDTO.java
+++ src/main/java/com/jojoldu/book/springboot/web/dto/PostsUpdateRequestDTO.java
@@ -6,12 +6,12 @@ import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
-public class PostsUpdateRequestDto {
+public class PostsUpdateRequestDTO {
   private String title;
   private String content;
 
   @Builder
-  public PostsUpdateRequestDto(String title, String content) {
+  public PostsUpdateRequestDTO(String title, String content) {
     this.title = title;
     this.content = content;
   }
